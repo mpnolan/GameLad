4aba24f819f540733f5c205f91cadf3f16d54bdf
Mike Nolan <mpnolan@truefitness.com>
2016-08-21 01:15:56 -0500

    Remove duplicate includes

--- gb-emu/videortspencoder.h
+++ gb-emu/videortspencoder.h
@@ -14,47 +14,17 @@ extern "C"
 #include <libavcodec/avcodec.h>
 #include <libavutil/imgutils.h>
 #include <libavutil/samplefmt.h>
-//#include <libavutil/timestamp.h>
 #include <libswscale/swscale.h>
-#include <libavcodec/avcodec.h>
 #include <libavformat/avformat.h>
-//#include <libavfilter/avfiltergraph.h>
-//#include <libavfilter/avcodec.h>
+#include <libavfilter/avfiltergraph.h>
 #include <libavfilter/buffersink.h>
 #include <libavfilter/buffersrc.h>
 #include <libavutil/opt.h>
-#include <libavutil/imgutils.h>
 #include <libavutil/mathematics.h>
-#include <libavutil/samplefmt.h>
 #include <libavutil/channel_layout.h>
 #include <libavutil/common.h>
 #include <libavutil/avstring.h>
-
-
-
 #include <math.h>
-#include <libavutil/opt.h>
-#include <libavcodec/avcodec.h>
-#include <libavutil/channel_layout.h>
-#include <libavutil/common.h>
-#include <libavutil/imgutils.h>
-#include <libavutil/mathematics.h>
-#include <libavutil/samplefmt.h>
-
-#include <libavcodec/avcodec.h>
-#include <libavformat/avformat.h>
-#include <libavfilter/avfiltergraph.h>
-//#include <libavfilter/avcodec.h>
-#include <libavfilter/buffersink.h>
-#include <libavfilter/buffersrc.h>
-#include <libavutil/opt.h>
-
-
-#include <libavutil/opt.h>
-#include <libavutil/mathematics.h>
-//#include <libavutil/timestamp.h>
-#include <libavformat/avformat.h>
-#include <libswscale/swscale.h>
 }
 
 using namespace std;
